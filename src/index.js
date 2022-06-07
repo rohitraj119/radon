91a1240a88b56ae4f312f1a7969f1b9a77e63b1a
dhirajuranium <dhirajpatil2311@gmail.com>
2022-06-07 10:17:01 +0530

    Day 16 asign

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/Dhiraj2198-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,28 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true
+    },
+
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+
+    year: {
+        type: Number,
+        default: 2021
+    },
+    
+    tags: [String],
+    authorName: String,
+    category: String,
+    totalPages: Number,
+    stockAvailable: Boolean,
+
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
