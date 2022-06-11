81fe4ce0e2ee214bf5153833eaa5e9c44a567e8f
rohit <rohitraj119@gmail.com>
2022-06-11 15:00:55 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -7,8 +7,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const midGlb = function (req,res,next){
+    console.log("hi I am a Global middleware");
+    next()
+}
+const assignmentMW = function (req,res,next){
+   var currentdate = new Date();
+   var datetime = "Last Sync: " + currentdate.getDay() + "/" + 
+   currentdate.getMonth()  + "/" 
+   + currentdate.getFullYear() + " @ " 
+   + currentdate.getHours() + ":" 
+   + currentdate.getMinutes() + ":"
+    + currentdate.getSeconds();
+    let ip = req.ip
+    let url= req.originalUrl
+    console.log(`${datetime} ${ip} ${url}`)
+    next()
+}
+app.use(assignmentMW)
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://functionup-cohort:qDrfepMoW4wUm4am@cluster0.qde1c.mongodb.net/rohit119-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -27,3 +46,4 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,10 +8,6 @@ const commonMW = require ("../middlewares/commonMiddlewares")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-
-
-
 router.post("/createBook", BookController.createBook  )
 
 
@@ -46,9 +42,9 @@ router.post("/createBook", BookController.createBook  )
 
 
 
+ 
 
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+router.get("/basicRoute", commonMW.mid1)
 
 
 
